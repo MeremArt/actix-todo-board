cece09b607a17e51c59d0ec3ac4f455c66dc9f7a
Ugo Chinemerem <ugofranklin22@gmail.com>
2025-04-23 13:51:12 -0400

    post request fix

--- src/todolist/services.rs
+++ src/todolist/services.rs
@@ -1,1 +1,24 @@
 use actix_web::{get,post,put,delete,web, Responder, HttpResponse};
+use crate::{AppState,TodolistEntry};
+use super::models::{CreateEntryData,UpdateEntryData};
+
+#[get("/todolist/entries")]
+async fn get_entries(data: web::Data<AppState>) -> impl Responder {
+    HttpResponse::Ok().json(data.todolist_entries.lock().unwrap().to_vec())
+}
+#[post("/todolist/entries")]
+async fn create_entry(data:wbe::Data<AppState>,param_obj:web::Json<CreateEntryData>) -> impl Responder {
+    let mut todolist_entries = data.todolist_entries.lock().unwrap();
+    let mut max_id:i32 = 0;
+    for i in 0..todolist_entries.len(){
+        if todolist_entries[i].id > max_id{
+            max_id = todolist_entries[i].id;
+        }
+    }
+  todolist_entries.push(TodolistEntry{
+        id:max_id+1,
+        date:param_obj.date,
+        title:param_obj.title.clone(),
+    });
+    HttpResponse::Ok().json(todolist_entries.to_vec())
+}
